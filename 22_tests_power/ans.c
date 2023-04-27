4fc6adc0f5740062df047fbbae15fdb5c083d615
mkx000 <3080233675@qq.com>
2023-04-27 21:42:10 +0800

    22 v2

---
+++ 22_tests_power/ans.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned power (unsigned x, unsigned y);
+
+int main() {
+  unsigned x;
+  unsigned y;
+  unsigned ans;
+
+  x = 2;
+  y = 0;
+  ans = 1;
+  if (ans != power(x, y)) {
+    return EXIT_FAILURE;
+  }
+  x = 1;
+  y = 0;
+  ans = 1;
+  if (ans != power(x, y)) {
+    return EXIT_FAILURE;
+  }
+
+  x = 1;
+  y = 1;
+  ans = 1;
+  if (ans != power(x, y)) {
+    return EXIT_FAILURE;
+  }
+
+  x = 2;
+  y = 1;
+  ans = 2;
+  if (ans != power(x, y)) {
+    return EXIT_FAILURE;
+  }
+
+  x = 2;
+  y = 3;
+  ans = 8;
+  if (ans != power(x, y)) {
+    return EXIT_FAILURE;
+  }
+
+  x = 3;
+  y = 3;
+  ans = 27;
+  if (ans != power(x, y)) {
+    return EXIT_FAILURE;
+  }
+
+  x = 1;
+  y = 3;
+  ans = 1;
+  if (ans != power(x, y)) {
+    return EXIT_FAILURE;
+  }
+
+  x = 0;
+  y = 0;
+  ans = 1;
+  if (ans != power(x, y)) {
+    return EXIT_FAILURE;
+  }
+
+  x = 3;
+  y = 2;
+  ans = 9;
+  if (ans != power(x, y)) {
+    return EXIT_FAILURE;
+  }
+
+  x = -3;
+  y = 2;
+  ans = 9;
+  if (ans != power(x, y)) {
+    return EXIT_FAILURE;
+  }
+
+  x = -2;
+  y = 3;
+  ans = -8;
+  if (ans != power(x, y)) {
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
